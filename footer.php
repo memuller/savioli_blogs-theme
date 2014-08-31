4c5f33c8fdb4f5cf0ba96bdda9368076a97d343b
Matheus E. Muller <hello@memuller.com>
2014-08-31 15:10:36 -0300

    Adds picolight.

---
+++ footer.php
@@ -0,0 +1,19 @@
+	</div>
+</div>
+	
+		<div id="footer_fundo">
+		<div id="wrapper2"><div id="footer">
+			<div class="left">&copy; <?php echo date('Y'); ?> - <?php bloginfo('name'); ?>. <?php _e('Todos os direitos reservados', 'picolight'); ?>.</div>
+			<div class="right">
+	
+				<div class="consultorweb">Consultoria:</div>
+				<div class="agenciapoint">Desenvolvido por:</div>
+	
+			</div>
+		</div></div>
+		</div>
+		
+
+		<?php wp_footer(); ?>
+</body>
+</html>
