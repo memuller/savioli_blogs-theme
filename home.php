66e341dacbcd16369b27299b4c4972f9ff997971
Matheus E. Muller <hello@memuller.com>
2015-02-23 03:45:41 -0300

    Assorted changes.

--- home.php
+++ home.php
@@ -1,8 +1,44 @@
-<?php get_header(); ?>
+<?php get_header(); $i = 1; global $posts; ?>
+	<section id="content">
+		<?php foreach($posts as $post): setup_postdata($post); ?>
+			<?php if($i % 2 != 0 && $i >= sizeof($posts)){ break; } ?>
+			<article class="grid col-half <?php post_class(); ?>" id="post-<?php the_ID()?>">
+				<div class="title">
+					<h2>
+						<a href="<?php the_permalink()?>">
+							<?php echo limit(get_the_title(), 40, ''); ?>
+						</a>
+					</h2>
+				</div>
+				<summary>
+					<div class="details">
+						<span class="categories">
+							<?php the_category() ?>
+						</span>
+						<span class="time">
+							<time><?php the_date('d/m/y') ?></time>		
+						</span>
+						<span class="comments">
+							<?php comments_number() ?>
+						</span>
+					</div>
+					<?php the_excerpt() ?>
+				</summary>
+			</article>
+			<?php $i++; ?>
+		<?php endforeach; ?>
+		<nav class="next">
+			<a href="<?php echo home_url('posts') ?>">
+				mais posts
+			</a>
+		</nav>
+	</section>
+
 	<section id="youtube" class="wrapper">
+		<h1>Vídeos</h1>
 		<?php foreach(\Savioli\Video::all(array('only' => 4)) as $video): ?>
 			<article>
-				<div class="entry-media"><?php echo wp_oembed_get($video->url, array('width' => 230, 'height' => 129)); ?></div>
+				<div class="entry-media"><?php echo wp_oembed_get($video->url, array('width' => 247)); ?></div>
 				<h2 class="entry-title">
 					<a href="<?php echo $video->url ?>">
 						<?php echo limit($video->title, 25, '') ?>
@@ -15,7 +51,7 @@
 		<?php endforeach; ?>
 	
 		<nav class="next">
-			<a href="<?php echo \Savioli\Video::channel_url() ?>">
+			<a href="<?php echo \Savioli\Video::channel_url(); ?>">
 				mais vídeos
 			</a>
 		</nav>
@@ -23,78 +59,15 @@
 
 	<?php $instagram_options = get_option('savioli_photo_options');?>
 	<?php if ($instagram_options['enabled']): ?>	
-		<section id="instafeed">
-		</section>
-	<?php endif ?>
-	
-	<section id="content">
-	<?php if (have_posts()) : ?>
-		<?php while (have_posts()) : the_post(); ?>
-			 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<div class="title">
-					<h2><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-				</div>
-				<div class="meta">
-				<div class="entry">
-					<?php if ( has_post_thumbnail() ) { ?>
-
-					<div class="thumbnail">
-						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
-							<?php the_post_thumbnail(); ?>
-						</a>
-					
-				</div>
-					<div class="indexexzerpt">
-						
-						<?php the_excerpt(); ?><br>
-						<a href="<?php the_permalink() ?>"><?php the_time('d. F Y') ?></a> &middot; <?php comments_popup_link(__('Deixe um comentário', 'picolight'), __('1 comentário', 'picolight'), __('% comentário', 'picolight')); ?>
-						
-					
-					<?php
-						picolight_show_categories();
-						picolight_show_tags();
-					?>
-				
-					<?php edit_post_link( __( '(Edit)', 'picolight' ), '<span class="edit-link">', '</span>' ); ?></div>
-			
-		
-					<?php }
-					else {
-						the_content(__('More &raquo;', 'picolight'));
-					} ?>
-					<?php if(wp_link_pages('echo=0') != "") { 
-						echo '<div class="pagelinks">';
-						wp_link_pages();
-						echo '</div>';
-					} ?>
-				</div>
-				</div>
-			</div>
-
-		<?php endwhile; ?>
-		<?php 
-		if(function_exists('wp_pagenavi')) { wp_pagenavi(); }
-		else { 
-		?>
-		<div class="navigation">
-			<div class="alignleft"><?php next_posts_link(__('&laquo;  Older articles', 'picolight')); ?></div>
-			<div class="alignright"><?php previous_posts_link(__('Newer articles &raquo;', 'picolight')); ?></div>
-		</div>
-
-		
-		<?php } ?>
-
-	<?php else : ?>
-
-		<div class="page" >
-			<h2><?php _e('Sorry, nothing found.', 'picolight'); ?></h2>
-			<div class="entry">		
-				<p><?php _e('It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching, or one of the links below, can help.', 'picolight' );?>.</p>
-			</div>
-		</div>
-
-	<?php endif; ?>
-
+	<section id="instagram">
+		<h1>Fotos</h1>
+		<div id="instafeed"></div>
+		<nav class="next">
+			<a href="<?php echo "http://instagram.com/".$instagram_options['user_name']; ?>">
+				mais fotos
+		</a>
+		</nav>
 	</section>
+	<?php endif ?>
 
 <?php get_footer(); ?>
--- lib/enqueue.php
+++ lib/enqueue.php
@@ -5,7 +5,7 @@
 
 add_action('wp_enqueue_scripts', function () {
 
-  wp_enqueue_style('main_css', get_stylesheet_directory_uri() . '/assets/main.min.css', false, '6c43141f45de2c2c342d7fca8502fe9b');
+  wp_enqueue_style('main_css', get_stylesheet_directory_uri() . '/assets/main.min.css', false, '4ea0a8ae2e2ef720b0bacf1649242f02');
 
   wp_register_script('main_scripts', get_stylesheet_directory_uri() . '/assets/main.min.js', array('jquery'), 'a3f436606a9c3c72c5c3f844b8ffbb58', true);
   wp_enqueue_script( 'main_scripts');
